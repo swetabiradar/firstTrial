5277204fd97606ff132ab3e355f151b527630768
Sweta <biradar.sweta@gmail.com>
2019-03-02 00:08:43 -0800

    removing file
    swp file

---
+++ graphs/buildCityGraphs.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+"""
+Created on Tue Jul 12 15:04:56 2016
+
+@author: guttag
+"""
+
+class Node(object):
+    def __init__(self, name):
+        """Assumes name is a string"""
+        self.name = name
+    def getName(self):
+        return self.name
+    def __str__(self):
+        return self.name
+
+class Edge(object):
+    def __init__(self, src, dest):
+        """Assumes src and dest are nodes"""
+        self.src = src
+        self.dest = dest
+    def getSource(self):
+        return self.src
+    def getDestination(self):
+        return self.dest
+    def __str__(self):
+        return self.src.getName() + '->' + self.dest.getName()
+               
+class Digraph(object):
+    """edges is a dict mapping each node to a list of
+    its children"""
+    def __init__(self):
+        self.edges = {}
+    def addNode(self, node):
+        print("In add node")
+        if node in self.edges:
+            print("In add node IF")
+            raise ValueError('Duplicate node')
+        else:
+            print("In add node ELSE")
+            self.edges[node] = []
+    def addEdge(self, edge):
+        src = edge.getSource()
+        dest = edge.getDestination()
+        if not (src in self.edges and dest in self.edges):
+            raise ValueError('Node not in graph')
+        self.edges[src].append(dest)
+    def childrenOf(self, node):
+        return self.edges[node]
+    def hasNode(self, node):
+        return node in self.edges
+    def getNode(self, name):
+        for n in self.edges:
+            if n.getName() == name:
+                return n
+        raise NameError(name)
+    def __str__(self):
+        result = ''
+        for src in self.edges:
+            for dest in self.edges[src]:
+                result = result + src.getName() + '->'\
+                         + dest.getName() + '\n'
+        return result[:-1] #omit final newline
+
+class Graph(Digraph):
+    def addEdge(self, edge):
+        Digraph.addEdge(self, edge)
+        rev = Edge(edge.getDestination(), edge.getSource())
+        Digraph.addEdge(self, rev)
+
+    
+def buildCityGraph(graphType):
+    g = graphType()
+    for name in ('Boston', 'Providence', 'New York', 'Chicago',
+                 'Denver', 'Phoenix', 'Los Angeles'): #Create 7 nodes
+        n = Node(name)
+        g.addNode(n)
+
+    print("I'm here" )
+    print("%s" % g.edges.keys())
+    for key in g.edges.keys():
+        print(key.name)
+    e1 = Edge(g.getNode('Boston'), g.getNode('Providence'))
+    g.addEdge(e1)
+    g.addEdge(Edge(g.getNode('Boston'), g.getNode('New York')))
+    g.addEdge(Edge(g.getNode('Providence'), g.getNode('Boston')))
+    g.addEdge(Edge(g.getNode('Providence'), g.getNode('New York')))
+    g.addEdge(Edge(g.getNode('New York'), g.getNode('Chicago')))
+    g.addEdge(Edge(g.getNode('Chicago'), g.getNode('Denver')))
+    g.addEdge(Edge(g.getNode('Denver'), g.getNode('Phoenix')))
+    g.addEdge(Edge(g.getNode('Denver'), g.getNode('New York')))
+    g.addEdge(Edge(g.getNode('Los Angeles'), g.getNode('Boston')))
+    return g
+    
+print(buildCityGraph(Graph))
+
---
+++ unique_morse_code_simplified.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+"""
+International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter. For example, "cba" can be written as "-.-..--...", (which is the concatenation "-.-." + "-..." + ".-"). We'll call such a concatenation, the transformation of a word.
+
+Return the number of different transformations among all words we have.
+
+Example:
+Input: words = ["gin", "zen", "gig", "msg"]
+Output: 2
+Explanation:
+The transformation of each word is:
+"gin" -> "--...-."
+"zen" -> "--...-."
+"gig" -> "--...--."
+"msg" -> "--...--."
+"""
+
+class Solution:
+    def __init__(self, words=[]):
+        self.words = words
+
+    def toStr(self):
+        return self.words
+
+    def uniqueMorseRepresentations(self):
+        print(self.words)
+        morse_list = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+
+        rep = {"".join(morse_list[ord(c)-ord('a')]for c in word)for word in self.words}
+        print(rep)
+        return (len(rep))
+
+words = ["gin", "zen", "gig", "msg"]
+a = Solution(words)
+print(a.uniqueMorseRepresentations())
